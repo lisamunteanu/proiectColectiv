35b456f0b2c4d337369dc4166c9aa83ef29e3c7e
Rigons <radumolnar1999@gmail.com>
2020-01-05 21:24:53 +0200

    trying to add remote repo

--- src/main/java/grupa235/proiectColectiv/smth/test.java
+++ src/main/java/grupa235/proiectColectiv/smth/test.java
@@ -2,6 +2,7 @@ package grupa235.proiectColectiv.smth;
 
 import java.io.InputStream;
 import java.util.Properties;
+import static com.sun.tools.javap.JavapTask.nl;
 
 public class test {
     public static void main(String[] args) {
