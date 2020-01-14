169fc3a60b442273ff0c0223f8f0da687025a018
lisamunteanu <lisamunteanu_i@yahoo.com>
2020-01-14 20:51:58 +0200

    User role for admin: 2; User role for user: 1- is set automatically when registering. Register is only for users, not for admin(s).

--- src/main/java/grupa235/proiectColectiv/frontendModel/UserDTO.java
+++ src/main/java/grupa235/proiectColectiv/frontendModel/UserDTO.java
@@ -3,6 +3,7 @@ package grupa235.proiectColectiv.frontendModel;
 public class UserDTO {
     private String username;
     private String password;
+    private String role;
 
     public String getUsername() {
         return username;
@@ -18,5 +19,13 @@ public class UserDTO {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
